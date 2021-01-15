e6b95444a9cc35023170dc298de6204a0ea05b16
rimisarK-blue <rimisark92@gmail.com>
2021-01-15 14:15:54 +0000

    updated

---
+++ FUNCTIONS/Exercise/Solutions/02. Add and Subtract.py
@@ -0,0 +1,8 @@
+def add_and_subtract(num1, num2, num3):
+    return (num1 + num2) - num3
+
+
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+print(add_and_subtract(number_1, number_2, number_3))
